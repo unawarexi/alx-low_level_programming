29c24f3893f00a4110d84b3e30cb9ff0a827c283
unawarexi <just.andrew001@gmail.com>
2023-02-16 16:46:36 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,8 @@
  * followed new line"
  * Return: Always 0.
  */
-
 int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);		
 }
