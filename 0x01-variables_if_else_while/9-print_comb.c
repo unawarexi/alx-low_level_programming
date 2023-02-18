9a6ff3d4f677a7644135fe9a11673cc62f348386
unawarexi <just.andrew001@gmail.com>
2023-02-18 11:11:53 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - "Entry point
+ * prints all possible combinations of single-digit numbers".
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 100; a++)
+	{
+		putchar(a + '0');
+		if (a < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
