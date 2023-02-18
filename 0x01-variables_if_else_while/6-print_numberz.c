0571b3a9c7d3f990966e108510504913f7653331
unawarexi <just.andrew001@gmail.com>
2023-02-18 10:21:33 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - "Entry point
+ * prints all single digit numbers of base 10 starting from 0 ".
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + 'a');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
