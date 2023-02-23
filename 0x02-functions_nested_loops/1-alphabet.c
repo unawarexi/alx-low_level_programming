9656d9c4722957515e07a4922ad74e75e652d094
unawarexi <just.andrew001@gmail.com>
2023-02-22 16:05:46 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - "check the code
+ * desc - Write a function that prints the alphabet, in lowercase"
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for
+		(i = 'a';
+		 i <= 'z';
+		 i++);
+	_putchar(i);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - "Write a function that prints the alphabet,
+ * in lowercase,  followed by a new line".
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
