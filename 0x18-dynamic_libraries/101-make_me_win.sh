11aa77e81fb6417fc17979df028ad0506d212797
unawarexi <unawarexi@gmail.com>
2023-08-28 03:38:24 -0700

    Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/unawarexi/alx-low_level_programming/raw/main/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=../win.so
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
